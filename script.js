414b18ec989e8f3b649916e4233363a19c0b1e00
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-04-20 17:01:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let inputBox = document.querySelector('#fname');
+
+inputBox.addEventListener('blur',convertUpperCase);
+
+function convertUpperCase() {
+	if(inputBox.value!="") {
+		let str = inputBox.value;
+		inputBox.value = str.toUpperCase();
+	}
+	
+}
